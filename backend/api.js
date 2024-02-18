c5ba2fbcf833cbe101369e481552c14f5bd1791c
Nicholas Pease <me@nicholaspease.com>
2024-02-18 01:28:40 +0000

    Webhook shell, ready for server testing

---
+++ backend/api.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+// Uses nodejs and express as "basic" backend server
+// Recieves webhook from Github at https://chatmaps.nicholaspease.com/api/v1/deploy
+app.post("/deploy", function (req, res) {
+    console.log(req.body)
+})
+
+app.listen(8000)
