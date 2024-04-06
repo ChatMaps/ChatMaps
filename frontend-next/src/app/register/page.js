d0c14cf1ab982b84de496b7e260c818dbd2b6325
Nicholas Pease <me@nicholaspease.com>
2024-04-06 17:55:11 -0400

    Login / Register Verification

--- frontend-next/src/app/register/page.js
+++ frontend-next/src/app/register/page.js
@@ -48,22 +48,33 @@ function Register() {
   var emailRegex =
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   var [passwordMismatch, setPasswordMismatch] = useState(false);
+  var [passwordLength, setPasswordLength] = useState(false);
   const passwordMatch = (data) => {
     return data.password === data.passwordCheck;
   };
+
+  const passwordLengthMatch = (data) => {
+    return data.password.length >= 6;
+  }
 
   /**
    * Form onSubmit Handler
    * @params {JSON} data - Form data
    */
   function onSubmit({ data }) {
-    if (passwordMatch(data)) {
+    if (passwordMatch(data) && passwordLengthMatch(data)) {
       setPasswordMismatch(false);
+      setPasswordLength(false)
       if (Signup(data)) {
         router.push("/onboarding");
       }
     } else {
-      setPasswordMismatch(true);
+      if (!passwordMatch(data)) {
+        setPasswordMismatch(true);
+      }
+      if (!passwordLengthMatch(data)) {
+        setPasswordLength(true);
+      }
       return;
     }
   }
@@ -112,6 +123,9 @@ function Register() {
               <br />
               {passwordMismatch && (
                 <p className="text-red-500">Passwords do not match</p>
+              )}
+              {passwordLength && (
+                <p className="text-red-500">Password must be at least 6 characters</p>
               )}
               <button
                 type="submit"
